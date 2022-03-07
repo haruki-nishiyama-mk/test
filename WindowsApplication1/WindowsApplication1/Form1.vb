3306c114e0d77fa94c1fd185223ef02e03f08d0a
haruki-nishiyama-mk <h-nishiyama@matusaka-kosan.co.jp>
2022-03-07 09:56:57 +0900

    追加　Helloworld!

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -2,5 +2,7 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
         lbHelloWorld.Text = "Hello World!"
+        lbHelloWorld.Text = "Hello World!"
+
     End Sub
 End Class
