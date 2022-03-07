18758e21f55eede64c0e126f1e2065fdec146176
haruki-nishiyama-mk <h-nishiyama@matusaka-kosan.co.jp>
2022-03-07 10:25:37 +0900

    日付表示作成

---
+++ Calculotar/Calculotar/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblNow = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'LblNow
+        '
+        Me.LblNow.AutoSize = True
+        Me.LblNow.Location = New System.Drawing.Point(103, 33)
+        Me.LblNow.Name = "LblNow"
+        Me.LblNow.Size = New System.Drawing.Size(38, 12)
+        Me.LblNow.TabIndex = 0
+        Me.LblNow.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.LblNow)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LblNow As System.Windows.Forms.Label
+
+End Class
---
+++ Calculotar/Calculotar/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Year関数で今日の西暦年号を取得 
+        Dim seireki As Integer = Year(Now)
+        ' Month関数で今日の月を取得
+        Dim tsuki As Integer = Month(Now)
+        ' day関数で今日の日付を取得
+        Dim hi As Integer = DateAndTime.Day(Now)
+        ' Hour関数で現在の時を取得
+        Dim ji As Integer = Hour(Now)
+        ' Minute関数で現在の分を取得
+        Dim hun As Integer = Minute(Now)
+        ' Sekond関数で現在の秒を取得
+        Dim Byou As Integer = Second(Now)
+
+        ' ラベルLblNowのTextプロパティに代入
+        LblNow.Text = seireki & "年"
+        LblNow.Text &= tsuki & "月"
+        LblNow.Text &= hi & "日"
+        LblNow.Text &= ji & "時"
+        LblNow.Text &= hun & "分"
+        LblNow.Text &= Byou & "秒"
+
+    End Sub
+End Class
