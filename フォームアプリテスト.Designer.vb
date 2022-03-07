82f5819583dfd39fbeb0e022bd5376cef2c4ea84
haruki-nishiyama-mk <h-nishiyama@matusaka-kosan.co.jp>
2022-03-07 09:22:44 +0900

    新規ファイル作成

---
+++ フォームアプリテスト.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClickThis = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnClickThis
+        '
+        Me.btnClickThis.Location = New System.Drawing.Point(0, 0)
+        Me.btnClickThis.Name = "btnClickThis"
+        Me.btnClickThis.Size = New System.Drawing.Size(75, 23)
+        Me.btnClickThis.TabIndex = 0
+        Me.btnClickThis.Text = "Click this"
+        Me.btnClickThis.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(303, 272)
+        Me.Controls.Add(Me.btnClickThis)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnClickThis As System.Windows.Forms.Button
+
+End Class
---
+++ フォームアプリテスト.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+        MessageBox("HelloWorld");
+    End Sub
+End Class
