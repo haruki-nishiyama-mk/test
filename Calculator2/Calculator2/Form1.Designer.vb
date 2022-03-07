0d349160b5033966ba08ddfa9d131f8a84de1c34
haruki-nishiyama-mk <h-nishiyama@matusaka-kosan.co.jp>
2022-03-07 10:41:59 +0900

    全種追記

---
+++ Calculator2/Calculator2/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblAnser = New System.Windows.Forms.Label()
+        Me.ButtonOne = New System.Windows.Forms.Button()
+        Me.ButtonTwo = New System.Windows.Forms.Button()
+        Me.ButtonThree = New System.Windows.Forms.Button()
+        Me.ButtonSix = New System.Windows.Forms.Button()
+        Me.Buttonfive = New System.Windows.Forms.Button()
+        Me.Buttonfour = New System.Windows.Forms.Button()
+        Me.ButtonNine = New System.Windows.Forms.Button()
+        Me.ButtonEight = New System.Windows.Forms.Button()
+        Me.ButtonSeven = New System.Windows.Forms.Button()
+        Me.ButtonZero = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LblAnser
+        '
+        Me.LblAnser.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LblAnser.Location = New System.Drawing.Point(30, 19)
+        Me.LblAnser.Name = "LblAnser"
+        Me.LblAnser.Size = New System.Drawing.Size(212, 37)
+        Me.LblAnser.TabIndex = 0
+        '
+        'ButtonOne
+        '
+        Me.ButtonOne.Location = New System.Drawing.Point(16, 190)
+        Me.ButtonOne.Name = "ButtonOne"
+        Me.ButtonOne.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonOne.TabIndex = 1
+        Me.ButtonOne.Text = "1"
+        Me.ButtonOne.UseVisualStyleBackColor = True
+        '
+        'ButtonTwo
+        '
+        Me.ButtonTwo.Location = New System.Drawing.Point(97, 190)
+        Me.ButtonTwo.Name = "ButtonTwo"
+        Me.ButtonTwo.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonTwo.TabIndex = 2
+        Me.ButtonTwo.Text = "2"
+        Me.ButtonTwo.UseVisualStyleBackColor = True
+        '
+        'ButtonThree
+        '
+        Me.ButtonThree.Location = New System.Drawing.Point(178, 190)
+        Me.ButtonThree.Name = "ButtonThree"
+        Me.ButtonThree.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonThree.TabIndex = 3
+        Me.ButtonThree.Text = "3"
+        Me.ButtonThree.UseVisualStyleBackColor = True
+        '
+        'ButtonSix
+        '
+        Me.ButtonSix.Location = New System.Drawing.Point(178, 154)
+        Me.ButtonSix.Name = "ButtonSix"
+        Me.ButtonSix.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSix.TabIndex = 6
+        Me.ButtonSix.Text = "6"
+        Me.ButtonSix.UseVisualStyleBackColor = True
+        '
+        'Buttonfive
+        '
+        Me.Buttonfive.Location = New System.Drawing.Point(97, 154)
+        Me.Buttonfive.Name = "Buttonfive"
+        Me.Buttonfive.Size = New System.Drawing.Size(75, 23)
+        Me.Buttonfive.TabIndex = 5
+        Me.Buttonfive.Text = "5"
+        Me.Buttonfive.UseVisualStyleBackColor = True
+        '
+        'Buttonfour
+        '
+        Me.Buttonfour.Location = New System.Drawing.Point(16, 154)
+        Me.Buttonfour.Name = "Buttonfour"
+        Me.Buttonfour.Size = New System.Drawing.Size(75, 23)
+        Me.Buttonfour.TabIndex = 4
+        Me.Buttonfour.Text = "4"
+        Me.Buttonfour.UseVisualStyleBackColor = True
+        '
+        'ButtonNine
+        '
+        Me.ButtonNine.Location = New System.Drawing.Point(178, 115)
+        Me.ButtonNine.Name = "ButtonNine"
+        Me.ButtonNine.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonNine.TabIndex = 9
+        Me.ButtonNine.Text = "9"
+        Me.ButtonNine.UseVisualStyleBackColor = True
+        '
+        'ButtonEight
+        '
+        Me.ButtonEight.Location = New System.Drawing.Point(97, 115)
+        Me.ButtonEight.Name = "ButtonEight"
+        Me.ButtonEight.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonEight.TabIndex = 8
+        Me.ButtonEight.Text = "8"
+        Me.ButtonEight.UseVisualStyleBackColor = True
+        '
+        'ButtonSeven
+        '
+        Me.ButtonSeven.Location = New System.Drawing.Point(16, 115)
+        Me.ButtonSeven.Name = "ButtonSeven"
+        Me.ButtonSeven.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSeven.TabIndex = 7
+        Me.ButtonSeven.Text = "7"
+        Me.ButtonSeven.UseVisualStyleBackColor = True
+        '
+        'ButtonZero
+        '
+        Me.ButtonZero.Location = New System.Drawing.Point(16, 222)
+        Me.ButtonZero.Name = "ButtonZero"
+        Me.ButtonZero.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonZero.TabIndex = 10
+        Me.ButtonZero.Text = "0"
+        Me.ButtonZero.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ButtonZero)
+        Me.Controls.Add(Me.ButtonNine)
+        Me.Controls.Add(Me.ButtonEight)
+        Me.Controls.Add(Me.ButtonSeven)
+        Me.Controls.Add(Me.ButtonSix)
+        Me.Controls.Add(Me.Buttonfive)
+        Me.Controls.Add(Me.Buttonfour)
+        Me.Controls.Add(Me.ButtonThree)
+        Me.Controls.Add(Me.ButtonTwo)
+        Me.Controls.Add(Me.ButtonOne)
+        Me.Controls.Add(Me.LblAnser)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents LblAnser As System.Windows.Forms.Label
+    Friend WithEvents ButtonOne As System.Windows.Forms.Button
+    Friend WithEvents ButtonTwo As System.Windows.Forms.Button
+    Friend WithEvents ButtonThree As System.Windows.Forms.Button
+    Friend WithEvents ButtonSix As System.Windows.Forms.Button
+    Friend WithEvents Buttonfive As System.Windows.Forms.Button
+    Friend WithEvents Buttonfour As System.Windows.Forms.Button
+    Friend WithEvents ButtonNine As System.Windows.Forms.Button
+    Friend WithEvents ButtonEight As System.Windows.Forms.Button
+    Friend WithEvents ButtonSeven As System.Windows.Forms.Button
+    Friend WithEvents ButtonZero As System.Windows.Forms.Button
+
+End Class
---
+++ Calculator2/Calculator2/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LblAnser.Click
+
+    End Sub
+
+    Private Sub ButtonOne_Click(sender As Object, e As EventArgs) Handles ButtonOne.Click
+        LblAnser.Text = "1"
+    End Sub
+
+    Private Sub ButtonTwo_Click(sender As Object, e As EventArgs) Handles ButtonTwo.Click
+        LblAnser.Text = "2"
+    End Sub
+
+    Private Sub ButtonThree_Click(sender As Object, e As EventArgs) Handles ButtonThree.Click
+        LblAnser.Text = "3"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonZero_Click(sender As Object, e As EventArgs) Handles ButtonZero.Click
+        LblAnser.Text = "0"
+
+    End Sub
+
+    Private Sub Buttonfour_Click(sender As Object, e As EventArgs) Handles Buttonfour.Click
+        LblAnser.Text = "4"
+
+    End Sub
+
+    Private Sub Buttonfive_Click(sender As Object, e As EventArgs) Handles Buttonfive.Click
+        LblAnser.Text = "5"
+
+    End Sub
+
+    Private Sub ButtonSix_Click(sender As Object, e As EventArgs) Handles ButtonSix.Click
+        LblAnser.Text = "6"
+
+    End Sub
+
+    Private Sub ButtonSeven_Click(sender As Object, e As EventArgs) Handles ButtonSeven.Click
+        LblAnser.Text = "7"
+
+    End Sub
+
+    Private Sub ButtonEight_Click(sender As Object, e As EventArgs) Handles ButtonEight.Click
+        LblAnser.Text = "8"
+
+    End Sub
+
+    Private Sub ButtonNine_Click(sender As Object, e As EventArgs) Handles ButtonNine.Click
+        LblAnser.Text = "9"
+
+    End Sub
+End Class
